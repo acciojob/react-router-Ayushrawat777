c9ac7367354905f8504a9664071a89252182242d
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-23 13:18:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
 import React from "react";
-import './../styles/App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <ul>
+      <li> <NavLink to="/">Home</NavLink></li> 
+      <li> <NavLink to="/about">About</NavLink></li> 
+      </ul>
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
