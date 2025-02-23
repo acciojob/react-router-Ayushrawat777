e061417958ef3fc54ea688aeb485925cf41bca4c
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-23 08:19:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./../styles/App.css";
 import Home from "./Home";
 import About from "./About";
@@ -8,11 +8,11 @@ import Navigation from "./Navigation";
 const App = () => {
   return (
     <Router>
-      <Navigation/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </Router>
   );
 };
